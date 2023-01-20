dc116986b6b7c4eadec010006c0a2b239eefa9e1
Barbara Pušnar <barbara.pusnar@scng.si>
2023-01-20 09:48:02 +0100

    vaja2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,22 @@
 ﻿class Program {
-    static void Main(string[] args) {      
-        
+    static void Main(string[] args)
+    {
+        Random stevilo=new Random();
+        int nak=stevilo.Next(1, 101);
+        int ugani = -1;
+        int stevec = 0;
+        while (nak != ugani)
+        {
+            Console.Write("Vnesi število: ");
+            ugani=int.Parse(Console.ReadLine());
+            stevec++;
+            if (ugani < nak)
+                Console.WriteLine("Število je premajhno.");
+            else if (ugani > nak)
+                Console.WriteLine("Število je preveliko.");
+        }
+        Console.WriteLine("Naključno število: "+nak);
+        Console.WriteLine("Bravo, uganil si.");
+        Console.WriteLine("Število poskusov: " + stevec);
     }
 }
---
+++ Vaja2/Vaja2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari Donald farmi ima");
+            Console.WriteLine("IAIAO");
+            Console.WriteLine("Na tej farmi osla dva");
+            Console.WriteLine("IAIAO");
+            Console.ReadLine();
+        }
+    }
+}
